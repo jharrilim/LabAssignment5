39313fee2e2803c40f4b496a60cdc078d74dc655
Kevin Ma <kbma.kevin@gmail.com>
2018-11-26 11:14:25 -0500

    Updated main screen to provide some app info to end users, act as bridge to 'First' activity in which inika wants us to use listview ctrls to display list of cuisines instead of btns

--- app/src/main/java/assignment5/comp304/josephharrisonlimkevinma/labassignment5/MainActivity.kt
+++ app/src/main/java/assignment5/comp304/josephharrisonlimkevinma/labassignment5/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import assignment5.comp304.josephharrisonlimkevinma.labassignment5.food.CuisineType
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,9 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //attach event handlers
+        findViewById<Button>(R.id.main_continue_btn).setOnClickListener { view -> this.onContinueBtnClick(view) }
     }
 
-    fun onMenuBtnClick(view: View?) {
+    fun onContinueBtnClick(view: View?) {
+
+        Toast.makeText(this, resources.getString(R.string.main_continue_btn_clicked_txt), Toast.LENGTH_LONG).show()
 
         fun startCuisineActivity(cuisineType: CuisineType) {
             val b = Bundle()
@@ -22,10 +29,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, RestaurantsListActivity::class.java).putExtras(b))
 //            startActivity(Intent(this, CuisineActivity::class.java).putExtras(b))
         }
-
-        when (view!!.id) {
-            startAmericanBtn.id -> startCuisineActivity(CuisineType.American)
-            startAsianBtn.id -> startCuisineActivity(CuisineType.Asian)
-        }
+//
+//        when (view!!.id) {
+//            startAmericanBtn.id -> startCuisineActivity(CuisineType.American)
+//            startAsianBtn.id -> startCuisineActivity(CuisineType.Asian)
+//        }
     }
 }
